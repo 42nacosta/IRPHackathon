28ebda5a08d73fd0c8d44abf277afd3575366323
42Nataco <nacosta31406@gmail.com>
2025-10-27 21:24:54 -0400

    Added about page and favicon

--- SoccerTracker/home/views.py
+++ SoccerTracker/home/views.py
@@ -1,13 +1,4 @@
 from django.shortcuts import render
-import matplotlib.pyplot as plt
-import numpy as np
-import requests
-import pandas as pd
-import json
-from mplsoccer import Pitch ,VerticalPitch
-from skillcorner.client import SkillcornerClient
-import streamlit as st
-
 
 # Create your views here.
 def index(request):
